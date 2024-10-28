4f3bd80f9de70a221fc705a58b750ee149f3db71
João Paulo <joaopaulojp12@gmail.com>
2024-10-28 16:07:33 -0300

    ajustes

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -15,6 +15,7 @@ export default function About({spot}: Props) {
 
                 <div className={styles.content}>
                     <h2>Sobre os Idealizadores</h2>
+                    
                     <p>O evento é organizado pelo Professor Renato Saraiva, referência em educação jurídica no Brasil, e conta com a visão estratégica de Jeff daCruz, especialista em audiovisual e responsável por transformar a maneira como advogados se posicionam no mundo digital. Além disso, diversos grandes nomes do conteúdo jurídico participarão para compartilhar suas melhores estratégias e experiências.</p>
                     <Button className={styles.btn} onClick={() => ScrollTo(spot)}>Garanta sua vaga agora</Button>
                 </div>
