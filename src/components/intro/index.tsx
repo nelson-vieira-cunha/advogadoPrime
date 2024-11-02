7755be02cb324a9770f2bb516d711a6802a29e82
João Paulo <joaopaulojp12@gmail.com>
2024-11-02 08:18:20 -0300

    ajustes

--- src/components/intro/index.tsx
+++ src/components/intro/index.tsx
@@ -43,66 +43,134 @@ export default function Intro({
     }, [])
 
     return (
-        <section className={styles.intro}>
-            <div className={`container`}>
+        // <section className={styles.intro}>
+        //     <div className={`container`}>
 
-                <div className={styles.video_container}>
-                    <Image
-                        className={styles.video_img}
-                        width={556}
-                        height={402}
-                        src="/header_landpage.jpg"
-                        alt="Imagem da equipe reunida."
-                    />
+        //         <div className={styles.video_container}>
+        //             <Image
+        //                 className={styles.video_img}
+        //                 width={556}
+        //                 height={402}
+        //                 src="/header_landpage.jpg"
+        //                 alt="Imagem da equipe reunida."
+        //             />
 
-                    <div 
-                        className={styles.video}
-                        onClick={() => {
-                            setYouTubeId('KC3OLb0ofME')
-                            setOpen(true)
-                        }}
-                    >
-                        <PlayIcon className={styles.play} />
-                        <Image
-                            className={styles.cover}
-                            width={556}
-                            height={315}
-                            src="/video-cover-record.jpg"
-                            alt="Imagem da equipe reunida."
-                        />
+        //             <div 
+        //                 className={styles.video}
+        //                 onClick={() => {
+        //                     setYouTubeId('KC3OLb0ofME')
+        //                     setOpen(true)
+        //                 }}
+        //             >
+        //                 <PlayIcon className={styles.play} />
+        //                 <Image
+        //                     className={styles.cover}
+        //                     width={556}
+        //                     height={315}
+        //                     src="/video-cover-record.jpg"
+        //                     alt="Imagem da equipe reunida."
+        //                 />
+        //             </div>
+
+        //         </div>
+
+
+        //         <div className={styles.content}>
+        //             <div>
+        //                 <h1>
+        //                     <span>Workshop imersivo presencial:</span> <br />
+        //                     Destravando sua Advocacia<br />
+        //                     Como se Tornar um Advogado Prime,<br /> Caruaru-PE
+        //                 </h1>
+        //                 <p className={styles.description}>Participe do evento que vai destravar sua advocacia e acelerar seu sucesso!<br />
+        //                 Com o Professor Renato Saraiva e seus convidados, você vai aprender as estratégias<br />
+        //                 comprovadas para dominar o mercado jurídico, se posicionar como autoridade nas redes sociais<br />
+        //                 e se destacar como um Advogado Prime – respeitado, valorizado e reconhecido.</p>
+        //                 <Button className={styles.btn} onClick={() => ScrollTo(spot)}>Garanta sua vaga agora</Button>
+        //             </div>
+
+        //             <div className={`${styles.progress_container} progress-container`}>
+        //                 <div className={`${styles.progress_content} progress-content`}>
+        //                     <ProgressBar
+        //                         className={styles.progress_bar}
+        //                         actualProgress={52 + 20}
+        //                         maxProgress={100}
+        //                         batch={1}
+        //                     />
+        //                 </div>
+        //             </div>
+
+        //         </div>
+
+        //     </div>
+        // </section>
+        <>
+            <section className={styles.intro}>
+                <div className={`container`}>
+
+                    <div className={styles.content}>
+                        <div>
+                            <h1>
+                                <span>Workshop imersivo presencial:</span> <br />
+                                Destravando sua Advocacia<br />
+                                Como se Tornar um Advogado Prime,<br /> Caruaru-PE
+                            </h1>
+                            <p className={styles.description}>
+                                Participe do evento que vai destravar sua advocacia e acelerar seu sucesso!<br />
+                                Com o Professor Renato Saraiva e seus convidados, você vai aprender as estratégias
+                                comprovadas para dominar o mercado jurídico, se posicionar como autoridade nas redes sociais
+                                e se destacar como um Advogado Prime – respeitado, valorizado e reconhecido.
+                            </p>
+                            <Button onClick={() => ScrollTo(spot)}>Garanta sua vaga agora</Button>
+                        </div>
+
+
+                        <div className={`${styles.progress_container} progress-container`}>
+                            <div className={`${styles.progress_content} progress-content`}>
+                                <ProgressBar
+                                    className={styles.progress_bar}
+                                    actualProgress={52 + 20}
+                                    maxProgress={100}
+                                    batch={1}
+                                />
+                            </div>
+                        </div>
+
+
                     </div>
 
                 </div>
+            </section>
 
-            
-                <div className={styles.content}>
-                    <div>
-                        <h1>
-                            <span>Workshop imersivo presencial:</span> <br />
-                            Destravando sua Advocacia<br />
-                            Como se Tornar um Advogado Prime,<br /> Caruaru-PE
-                        </h1>
-                        <p className={styles.description}>Participe do evento que vai destravar sua advocacia e acelerar seu sucesso!<br />
-                        Com o Professor Renato Saraiva e seus convidados, você vai aprender as estratégias<br />
-                        comprovadas para dominar o mercado jurídico, se posicionar como autoridade nas redes sociais<br />
-                        e se destacar como um Advogado Prime – respeitado, valorizado e reconhecido.</p>
-                        <Button className={styles.btn} onClick={() => ScrollTo(spot)}>Garanta sua vaga agora</Button>
-                    </div>
+            <section>
+                <div className={`container`}>
 
-                    <div className={`${styles.progress_container} progress-container`}>
-                        <div className={`${styles.progress_content} progress-content`}>
-                            <ProgressBar
-                                className={styles.progress_bar}
-                                actualProgress={52 + 20}
-                                maxProgress={100}
-                                batch={1}
+                    <div className={styles.video_container}>
+                        
+
+                        <div
+                            className={styles.video}
+                            onClick={() => {
+                                setYouTubeId('KC3OLb0ofME')
+                                setOpen(true)
+                            }}
+                        >
+                            <PlayIcon className={styles.play} />
+                            <Image
+                                className={styles.cover}
+                                width={556}
+                                height={315}
+                                src="/video-cover-record.jpg"
+                                alt="Imagem da equipe reunida."
                             />
                         </div>
+
                     </div>
+                    </div>
+            </section>
+        </>
 
-                </div>
 
-            </div>
-        </section>
+
     )
 }
